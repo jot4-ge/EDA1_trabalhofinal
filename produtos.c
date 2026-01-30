2ebfbd889eec1766d2b3a41462c3f1ceb2ee733c
Heitorovski01 <heitorbsbdf@gmail.com>
2026-01-30 00:57:49 -0300

    feat: Função Listar Produtos

--- produtos.c
+++ produtos.c
@@ -20,3 +20,25 @@ Produto *adicionarProduto(Produto *lista, int codigo, char *nome, float preco, i
 
     return novo;
 }
+
+void listarProdutos(Produto *lista){
+    Produto *le = lista;
+
+    if (le == NULL) {
+        printf("\n>> A lista está vazia no momento.\n");
+        return;
+    }
+
+    printf("\n=== Lista de Produtos ===\n");
+
+    while(le != NULL){
+        printf("--------------------------------\n");
+        printf("Nome do produto: %s\n", le -> nome);
+        printf("Código do produto: %d\n", le -> codigo);
+        printf("Preço do produto: R$ %.2f\n", le -> preco);
+        printf("Quantidade no estoque do produto: %d\n", le -> quantidade_estoque);
+
+        le = le -> prox;
+    }
+    printf("--------------------------------\n");
+}
